a1ae09e4ddead7fb2f18ec0d1b41a1ddced08962
Can Kologlu <mckologlu@gmail.com>
2023-01-19 19:30:53 -0500

    modification to breedfetcher with callback funct and m&c test

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,25 +3,21 @@ const fs = require("fs");
 const query = process.argv.slice(2);
 const url = "https://api.thecatapi.com/v1/breeds/search?q="
 
-request (url + query,(error, response, body) => {
+const fetchBreedDescription = (breedName, callBack) => {
 
-
-  // console.log(typeof body);
-  // console.log("error is:", error)
-  // console.log("statusCode:", response && response.statusCode);
-  // console.log("body:", body[0]);
-  if (error) {
-    console.log("Something went wrong!")
-  }
+request (url + breedName,(error, response, body) => {
   let result = JSON.parse(body);
+  if (error) {
+    callBack("Something went wrong!")
+  }
   if(result.length === 0) {
-    console.log("Breed not found!")
+    callBack("Breed not found!")
   } else {
-    console.log(result);
-    console.log(typeof result);
-    console.log(result[0].name)
+    callBack(null, result[0].description)
    }
+});
+}
 
-  
+module.exports = {fetchBreedDescription};
 
-} )
+// console.log(module);
